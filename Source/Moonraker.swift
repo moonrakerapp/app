91cbb27abb7a7e1ac3e8e54f7a9ef8dde4d29272
vauxhall <zero.griffin@gmail.com>
2020-01-01 10:47:27 +0100

    Get sun coordinates

--- Source/Moonraker.swift
+++ Source/Moonraker.swift
@@ -4,6 +4,11 @@ import Combine
 public final class Moonraker {
     public let visible = CurrentValueSubject<Double, Never>(0)
     private let queue = DispatchQueue(label: "", qos: .background, target: .global(qos: .background))
+    private let J1970 = Double(2440588)
+    private let J2000 = Double(2451545)
+    private let radians = Double.pi / 180
+    private let earthPerihelion = Double.pi / 180 * 102.9372
+    private let earthObliquity = Double.pi / 180 * 23.4397
     
     public init() {
         
@@ -15,8 +20,39 @@ public final class Moonraker {
         }
     }
     
-    func visible(_ timestamp: TimeInterval) -> Double {
+    func visible(_ time: TimeInterval) -> Double {
+        let _days = days(time)
         return 0
+    }
+    
+    func days(_ time: TimeInterval) -> Double {
+        (time / (60 * 60 * 24)) - 0.5 + J1970 - J2000
+    }
+    
+    func sunCoords(_ days: Double) -> (Double, Double) {
+        {
+            (declination(0, $0), rightAscension(0, $0))
+        } (eclipticLongitude(solarMeanAnomaly(days)))
+    }
+    
+    func solarMeanAnomaly(_ days: Double) -> Double {
+        radians * (357.5291 + 0.98560028 * days)
+    }
+    
+    func eclipticLongitude(_ anomaly: Double) -> Double {
+        anomaly + equationOfCenter(anomaly) + earthPerihelion + .pi
+    }
+    
+    func declination(_ latitude: Double, _ longitude: Double) -> Double {
+        asin(sin(latitude) * cos(earthObliquity) + cos(latitude) * sin(earthObliquity) * sin(longitude))
+    }
+    
+    func rightAscension(_ latitude: Double, _ longitude: Double) -> Double {
+        atan2(sin(longitude) * cos(earthObliquity) - tan(latitude) * sin(earthObliquity), cos(longitude))
+    }
+    
+    private func equationOfCenter(_ anomaly: Double) -> Double {
+        radians * (1.9148 * sin(anomaly) + 0.02 * sin(2 * anomaly) + 0.0003 * sin(3 * anomaly))
     }
 }
 
--- Tests/TestVisible.swift
+++ Tests/TestVisible.swift
@@ -11,4 +11,34 @@ final class TestVisible: XCTestCase {
     func testVisible() {
         XCTAssertEqual(0.5490432602177777, moonraker.visible(1300000000))
     }
+    
+    func testDays() {
+        XCTAssertEqual(4088.7962962961756, moonraker.days(1300000000))
+    }
+    
+    func testSunDeclination() {
+        XCTAssertEqual(0.09136648771862077, moonraker.sunCoords(5000).0)
+    }
+    
+    func testSunRightAscension() {
+        XCTAssertEqual(2.928664200072675, moonraker.sunCoords(5000).1)
+    }
+    
+    func testSolarMeanAnomaly() {
+        XCTAssertEqual(92.24990993958214, moonraker.solarMeanAnomaly(5000))
+    }
+    
+    func testEclipticLongitude() {
+        XCTAssertEqual(97.15794246793237, moonraker.eclipticLongitude(92.24990993958214))
+    }
+    
+    func testDeclination() {
+        XCTAssertEqual(0.09136648771862077, moonraker.declination(0, 97.15794246793237))
+    }
+    
+    func testRightAscension() {
+        XCTAssertEqual(2.928664200072675, moonraker.rightAscension(0, 97.15794246793237))
+    }
+    
+    
 }

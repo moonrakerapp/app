670104a8f8df098dfde2f4076a7fc3bd95f6c6cc
vauxhall <zero.griffin@gmail.com>
2020-01-07 13:39:24 +0100

    Update pop over

--- Mac/Pop.swift
+++ Mac/Pop.swift
@@ -1,46 +1,31 @@
 import Moonraker
 import AppKit
+import Combine
 
 final class Pop: NSPopover {
-    var info: Info! {
-        didSet {
-            phase.stringValue = .key("Phase.\(info.phase)")
-            percent.attributed([("\(Float(Int(info.fraction * 100000)) / 1000)", .bold(16), .textColor),
-                                ("%", .regular(14), .textColor)])
-            
-            let animation = CABasicAnimation(keyPath: "strokeEnd")
-            animation.duration = 2
-            animation.fromValue = illumination.strokeEnd
-            animation.toValue = .init(info.fraction)
-            animation.timingFunction = .init(name: .easeOut)
-            illumination.strokeEnd = .init(info.fraction)
-            illumination.add(animation, forKey: "strokeEnd")
-        }
-    }
-    
-    private weak var phase: Label!
-    private weak var percent: Label!
-    private weak var illumination: CAShapeLayer!
+    private var info: AnyCancellable!
     
     required init?(coder: NSCoder) { nil }
     override init() {
         super.init()
-        contentSize = .init(width: 240, height: 300)
+        contentSize = .init(width: 240, height: 140)
         contentViewController = .init()
         contentViewController!.view = .init()
         behavior = .transient
         
-        let phase = Label("", .bold(20), .textColor)
+        let phase = Label("", .medium(14), .textColor)
         contentViewController!.view.addSubview(phase)
-        self.phase = phase
-        
-        let _illumination = Label(.key("Pop.illumination"), .regular(16), .textColor)
-        contentViewController!.view.addSubview(_illumination)
         
         let percent = Label([])
-        percent.alphaValue = 0.4
+        percent.alphaValue = 0.5
         contentViewController!.view.addSubview(percent)
-        self.percent = percent
+        
+        let date = Label("", .regular(14), .textColor)
+        contentViewController!.view.addSubview(date)
+        
+        let formatter = DateFormatter()
+        formatter.dateStyle = .short
+        formatter.timeStyle = .short
         
         let container = NSView()
         container.translatesAutoresizingMaskIntoConstraints = false
@@ -61,50 +46,37 @@ final class Pop: NSPopover {
         } (CGMutablePath())
         illumination.strokeEnd = 0
         container.layer!.addSublayer(illumination)
-        self.illumination = illumination
         
-        let base = NSView()
-        base.translatesAutoresizingMaskIntoConstraints = false
-        base.wantsLayer = true
-        contentViewController!.view.addSubview(base)
+        info = moonraker.info.receive(on: DispatchQueue.main).sink {
+            phase.stringValue = .key("Phase.\($0.phase)")
+            percent.attributed([("\(Int(round($0.fraction * 1000) / 10))", .bold(16), .textColor),
+                                ("%", .regular(14), .textColor)])
+            
+            let animation = CABasicAnimation(keyPath: "strokeEnd")
+            animation.duration = 1
+            animation.fromValue = illumination.strokeEnd
+            animation.toValue = .init($0.fraction)
+            animation.timingFunction = .init(name: .easeOut)
+            illumination.strokeEnd = .init($0.fraction)
+            illumination.add(animation, forKey: "strokeEnd")
+            
+            if moonraker.offset != 0 {
+                date.stringValue = formatter.string(from: moonraker.date.addingTimeInterval(moonraker.offset))
+            }
+        }
         
-        let moon = Moon()
-//        moon.radius = 5
-        moon.center = .init(x: 50, y: 50)
-        moon.resize()
-        moon.update()
-        base.layer!.addSublayer(moon)
-        
-        base.topAnchor.constraint(equalTo: contentViewController!.view.topAnchor, constant: 20).isActive = true
-        base.leftAnchor.constraint(equalTo: contentViewController!.view.leftAnchor, constant: 20).isActive = true
-        base.widthAnchor.constraint(equalToConstant: 200).isActive = true
-        base.heightAnchor.constraint(equalToConstant: 60).isActive = true
-        
-        phase.topAnchor.constraint(equalTo: contentViewController!.view.topAnchor, constant: 100).isActive = true
+        phase.topAnchor.constraint(equalTo: contentViewController!.view.topAnchor, constant: 40).isActive = true
         phase.leftAnchor.constraint(equalTo: contentViewController!.view.leftAnchor, constant: 20).isActive = true
         
-        _illumination.topAnchor.constraint(equalTo: phase.bottomAnchor, constant: 20).isActive = true
-        _illumination.leftAnchor.constraint(equalTo: phase.leftAnchor).isActive = true
-        
-        percent.topAnchor.constraint(equalTo: _illumination.topAnchor).isActive = true
+        percent.topAnchor.constraint(equalTo: phase.topAnchor).isActive = true
         percent.rightAnchor.constraint(equalTo: contentViewController!.view.rightAnchor, constant: -20).isActive = true
         
+        date.topAnchor.constraint(equalTo: container.bottomAnchor, constant: 15).isActive = true
+        date.centerXAnchor.constraint(equalTo: container.centerXAnchor).isActive = true
+        
         container.leftAnchor.constraint(equalTo: phase.leftAnchor).isActive = true
-        container.topAnchor.constraint(equalTo: _illumination.bottomAnchor, constant: 6).isActive = true
+        container.topAnchor.constraint(equalTo: phase.bottomAnchor, constant: 15).isActive = true
         container.heightAnchor.constraint(equalToConstant: 10).isActive = true
         container.widthAnchor.constraint(equalToConstant: 200).isActive = true
-    }
-    
-    @objc private func update() {
-//        let animation = CABasicAnimation(keyPath: "strokeEnd")
-//        animation.duration = 0.3
-//        animation.fromValue = illumination.strokeEnd
-//        animation.toValue = 0
-//        illumination.strokeEnd = 0
-//        illumination.add(animation, forKey: "strokeEnd")
-//        
-//        DispatchQueue.main.asyncAfter(deadline: .now() + 0.3) {
-//            (NSApp as! App).update()
-//        }
     }
 }

427b1269e086aa13a5dfd92e9a6f6358b741c5fa
vauxhall <zero.griffin@gmail.com>
2020-01-11 12:22:49 +0100

    Configure watch

---
+++ Watch/App.swift
@@ -0,0 +1,11 @@
+import WatchKit
+
+final class App: NSObject, WKExtensionDelegate {
+    override init() {
+        super.init()
+    }
+    
+    func applicationDidBecomeActive() {
+
+    }
+}
---
+++ Watch/Horizon.swift
@@ -0,0 +1,5 @@
+import SwiftUI
+
+final class Horizon: WKHostingController<Circle> {
+    override var body: Circle { .init() }
+}
